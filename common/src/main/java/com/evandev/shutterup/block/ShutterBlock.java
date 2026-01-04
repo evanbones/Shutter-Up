b828a079eb6d38042fb8166fddd8eb3c8d77afac
evanbones <74276616+evanbones@users.noreply.github.com>
2026-01-03 21:08:03 -0800

    Clickthrough backport

--- common/src/main/java/com/evandev/shutterup/block/ShutterBlock.java
+++ common/src/main/java/com/evandev/shutterup/block/ShutterBlock.java
@@ -125,6 +125,10 @@ public class ShutterBlock extends Block implements SimpleWaterloggedBlock {
 
     @Override
     public @NotNull InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
+        return toggleShutter(state, level, pos, player);
+    }
+
+    public InteractionResult toggleShutter(BlockState state, Level level, BlockPos pos, Player player) {
         state = state.cycle(OPEN);
         state = calculateBlockedState(state, level, pos);
         level.setBlock(pos, state, 10);
